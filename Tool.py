da9590487dfe1b26b613fa039a4ea494b92e7481
bmahabirbu <bmahabir@bu.edu>
2021-03-24 23:19:49 -0400

    updated tool.py

--- Tool.py
+++ Tool.py
@@ -1,18 +1,38 @@
+import math
+import sys
+from os import listdir
+from os.path import isfile, join
 from itertools import combinations
 from celloapi2 import CelloQuery, CelloResult
 
 # Set our directory variables. If you have a Windows based
 # operating system you need to input window based paths.
-in_dir = '/home/jackson/cello/input'
-out_dir = '/home/jackson/cello/output'
-v_file = 'and.v'
+in_dir = '/home/brian/ec-552-hw1/input'
+out_dir = '/home/brian/ec-552-hw1/output'
+
+# get all filenames in the input directory and put them in a list
+onlyfiles = [f for f in listdir("/home/brian/ec-552-hw1/input") if isfile(join("/home/brian/ec-552-hw1/input", f))]
+#ask user to input file with error checking
+while True:
+    v_file_input = input("enter verilog file with .v you want to optimize\n")
+    if v_file_input in onlyfiles:
+        v_file = v_file_input
+        break
+    elif v_file_input == 'q':
+        print("you have quit")
+        sys.exit(5)
+    else :
+        print(v_file_input + " not a valid input try again if you want to quit press q")
+
 options = 'options.csv'
 # Calculate number of inputs into the Circuit.
 signal_input = 2
 
 # We want to try every e-coli chassis.
-chassis_name = ['Eco1C1G1T1', 'Eco1C2G2T2', 'Eco2C1G3T1']
+chassis_name = ['Eco1C1G1T1']
+#Eco1C2G2T2', 'Eco2C1G3T1']
 
+#Have muliple tests for each input file edit
 best_score = 0
 best_chassis = None
 best_input_signals = None
